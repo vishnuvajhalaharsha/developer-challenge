df2b6d42ccd31f425647a81553bf8b93b4d8d7a1
Sri Harsha <vass.harsha@gmail.com>
2024-06-03 02:48:27 +0100

    get balances api updated, formattted code

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,59 +3,64 @@ import bodyparser from "body-parser";
 import express from "express";
 import simplestorage from "../../solidity/artifacts/contracts/simple_storage.sol/SimpleStorage.json";
 import token from "../../solidity/artifacts/contracts/token.sol/Token.json";
-import reviewstorage from "../../solidity/artifacts/contracts/review_storage.sol/ReviewStorage.json";
 import rewardToken from "../../solidity/artifacts/contracts/review_token.sol/RewardToken.json";
-import { ethers } from "ethers";
 import mongoose from "mongoose";
-import User from "./models/User";
-import  Rating from "./models/MovieRating";
-
+import Rating from "./models/MovieRating";
 
 const PORT = 4001;
 const HOST = "http://localhost:8000";
 const NAMESPACE = "default";
 const SIMPLE_STORAGE_ADDRESS = "0x33221CeF190bEC4fcD58d287ae1A07A8a761EbBd";
 const TOKEN_ADDRESS = "0x4a2b84c4F572515552f32B2a0725b1029b5f76e8";
-const REVIEW_ADDRESS="0xF2245cF71fAef6f4C346416c1EaFDb67322c2CAa"
-const REWARD_ADDRESS="0xaf73471f0234FEE64A629c9AC5c17776C37721c3"
+
+const REWARD_ADDRESS = "0xaf73471f0234FEE64A629c9AC5c17776C37721c3";
 const app = express();
 const firefly = new FireFly({
   host: HOST,
   namespace: NAMESPACE,
 });
-const MONGO_URI = 'mongodb://localhost:27017/MovieReviews';
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true } as mongoose.ConnectOptions)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
-
-
+const MONGO_URI = "mongodb://localhost:27017/MovieReviews";
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  } as mongoose.ConnectOptions)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 const ffiAndApiVersion = 2;
 const ssFfiName: string = `simpleStorageAndReviewFFI-${ffiAndApiVersion}`;
 const ssApiName: string = `simpleStorageAndReviewApi-${ffiAndApiVersion}`;
 const tokenFfiName: string = `tokenFFI-${ffiAndApiVersion}`;
 const tokenApiName: string = `tokenApi-${ffiAndApiVersion}`;
-const rewardName:string =`rewardStorageFFI-${ffiAndApiVersion}`
-const rewardApiName:string =`rewardStorageApi-${ffiAndApiVersion}`
-
-
-
+const rewardName: string = `rewardStorageFFI-${ffiAndApiVersion}`;
+const rewardApiName: string = `rewardStorageApi-${ffiAndApiVersion}`;
 
 app.use(bodyparser.json());
+// get balance of tokens for eth address
+app.get("/api/bal/:ethId", async (req, res) => {
+  const { ethId } = req.params;
 
-app.get('/api/bal', async (req, res) => {
-  const balance = await firefly.queryContractAPI(rewardApiName, 'balanceOf',
-    // Generate a unique idempotency key if needed
-  {  input: { account: '0x17d596f367e3f6a6fba8d0ae4f4bbad2fc07c0cc' },
-    key: '0x17d596f367e3f6a6fba8d0ae4f4bbad2fc07c0cc',
-    options: {}}
-  );
-  res.status(200).json({ balance });
+  if (!ethId) {
+    return res.status(400).json({ error: "Ethereum address is required" });
+  }
 
+  try {
+    const balance = await firefly.queryContractAPI(rewardApiName, "balanceOf", {
+      input: { account: ethId },
+      key: ethId,
+      options: {},
+    });
+
+    res.status(200).json({ balance });
+  } catch (error) {
+    console.error("Failed to query balance:", error);
+    res.status(500).json({ error: "Failed to retrieve balance" });
+  }
 });
 
-
-app.post('/api/movie-rating', async (req, res) => {
+// store movie review in DB,  store movie id, rating, review, on chain and mint token as reward
+app.post("/api/movie-rating", async (req, res) => {
   const { ethId, movieId, rating, review } = req.body;
 
   const newRating = new Rating({ ethId, movieId, rating, review });
@@ -64,93 +69,47 @@ app.post('/api/movie-rating', async (req, res) => {
     await newRating.save();
 
     // Interact with blockchain via FireFly to add review
-    const fireflyRes = await firefly.invokeContractAPI(ssApiName, 'addReview', {
+    const fireflyRes = await firefly.invokeContractAPI(ssApiName, "addReview", {
       input: {
         movieId,
         rating,
         review,
       },
-      key: ethId 
+      key: ethId,
     });
 
-    console.log('Review stored on blockchain');
-  
+    console.log("Review stored on blockchain");
 
     newRating.blockchainTxId = fireflyRes.id;
-     await firefly.sendBroadcast({
-      header: {
-      },
-      data: [
-        { value: fireflyRes.id },
-      ],
+    await firefly.sendBroadcast({
+      header: {},
+      data: [{ value: fireflyRes.id }],
     });
-    const rewardAmount = 1 * (10 ** 18); 
-     await firefly.invokeContractAPI(rewardApiName,'mint',{
-      input:{
-        amount:rewardAmount
+    const rewardAmount = 1 * 10 ** 18;
+    await firefly.invokeContractAPI(rewardApiName, "mint", {
+      input: {
+        amount: rewardAmount,
       },
-      key:ethId
-     })
-
+      key: ethId,
+    });
 
     await newRating.save();
-    console.log('Blockchain transaction ID saved to MongoDB');
+    console.log("Blockchain transaction ID saved to MongoDB");
 
-    res.status(201).json({ message: 'Rating submitted successfully and stored on blockchain', transactionHash: fireflyRes.id });
-
-  } catch (error:any) {
+    res
+      .status(201)
+      .json({
+        message: "Rating submitted successfully and stored on blockchain",
+        transactionHash: fireflyRes.id,
+      });
+  } catch (error: any) {
     if (error.code === 11000) {
-      // Duplicate key error
-      res.status(400).json({ error: 'You have already rated this movie.' });
+      res.status(400).json({ error: "You have already rated this movie." });
     } else {
-      // Other errors
-      res.status(500).json({ error: 'An error occurred while submitting your rating.' });
+      res
+        .status(500)
+        .json({ error: "An error occurred while submitting your rating." });
     }
-  }
-});
-
-
-app.get("/api/value", async (req, res) => {
-  res.send(await firefly.queryContractAPI(ssApiName, "get", {}));
-});
-
-app.post("/api/value", async (req, res) => {
-  try {
-    const fireflyRes = await firefly.invokeContractAPI(ssApiName, "set", {
-      input: {
-        x: req.body.x,
-      },
-    });
-    res.status(202).send({
-      id: fireflyRes.id,
-    });
-    /* eslint-disable  @typescript-eslint/no-explicit-any */
-  } catch (e: any) {
-    res.status(500).send({
-      error: e.message,
-    });
-  }
-});
-
-app.post("/api/mintToken", async (req, res) => {
-  try {
-    const fireflyRes = await firefly.invokeContractAPI(
-      tokenApiName,
-      "safeMint",
-      {
-        input: {
-          tokenId: Number(req.body.tokenId),
-        },
-      }
-    );
-    res.status(202).send({
-      tokenId: fireflyRes.input.input.tokenId,
-    });
-    /* eslint-disable  @typescript-eslint/no-explicit-any */
-  } catch (e: any) {
-    res.status(500).send({
-      error: e.message,
-    });
   }
 });
 
@@ -166,13 +125,13 @@ async function init() {
         abi: simplestorage.abi,
       },
     })
-    .then(async (ssGeneratedFFI:any) => {
+    .then(async (ssGeneratedFFI: any) => {
       if (!ssGeneratedFFI) return;
       return await firefly.createContractInterface(ssGeneratedFFI, {
         confirm: true,
       });
     })
-    .then(async (ssContractInterface:any) => {
+    .then(async (ssContractInterface: any) => {
       if (!ssContractInterface) return;
       return await firefly.createContractAPI(
         {
@@ -187,11 +146,14 @@ async function init() {
         { confirm: true }
       );
     })
-    .catch((e:any) => {
+    .catch((e: any) => {
       const err = JSON.parse(JSON.stringify(e.originalError));
 
       if (err.status === 409) {
-        console.log("'simpleStorageFFI' already exists in FireFly. Ignoring.", err);
+        console.log(
+          "'simpleStorageFFI' already exists in FireFly. Ignoring.",
+          err
+        );
       } else {
         return;
       }
@@ -208,13 +170,13 @@ async function init() {
         abi: token.abi,
       },
     })
-    .then(async (tokenGeneratedFFI:any) => {
+    .then(async (tokenGeneratedFFI: any) => {
       if (!tokenGeneratedFFI) return;
       return await firefly.createContractInterface(tokenGeneratedFFI, {
         confirm: true,
       });
     })
-    .then(async (tokenContractInterface:any) => {
+    .then(async (tokenContractInterface: any) => {
       if (!tokenContractInterface) return;
       return await firefly.createContractAPI(
         {
@@ -229,7 +191,7 @@ async function init() {
         { confirm: true }
       );
     })
-    .catch((e:any) => {
+    .catch((e: any) => {
       const err = JSON.parse(JSON.stringify(e.originalError));
 
       if (err.status === 409) {
@@ -239,49 +201,47 @@ async function init() {
       }
     });
 
-
   //REWARD
   await firefly
-  .generateContractInterface({
-    name: rewardName,
-    namespace: NAMESPACE,
-    version: "1.0",
-    description: "Deployed token contract",
-    input: {
-      abi: rewardToken.abi,
-    },
-  })
-  .then(async (rtokenGeneratedFFI:any) => {
-    if (!rtokenGeneratedFFI) return;
-    return await firefly.createContractInterface(rtokenGeneratedFFI, {
-      confirm: true,
-    });
-  })
-  .then(async (rtokenContractInterface:any) => {
-    if (!rtokenContractInterface) return;
-    return await firefly.createContractAPI(
-      {
-        interface: {
-          id: rtokenContractInterface.id,
-        },
-        location: {
-          address: REWARD_ADDRESS,
-        },
-        name: rewardApiName,
+    .generateContractInterface({
+      name: rewardName,
+      namespace: NAMESPACE,
+      version: "1.0",
+      description: "Deployed token contract",
+      input: {
+        abi: rewardToken.abi,
       },
-      { confirm: true }
-    );
-  })
-  .catch((e:any) => {
-    const err = JSON.parse(JSON.stringify(e.originalError));
+    })
+    .then(async (rtokenGeneratedFFI: any) => {
+      if (!rtokenGeneratedFFI) return;
+      return await firefly.createContractInterface(rtokenGeneratedFFI, {
+        confirm: true,
+      });
+    })
+    .then(async (rtokenContractInterface: any) => {
+      if (!rtokenContractInterface) return;
+      return await firefly.createContractAPI(
+        {
+          interface: {
+            id: rtokenContractInterface.id,
+          },
+          location: {
+            address: REWARD_ADDRESS,
+          },
+          name: rewardApiName,
+        },
+        { confirm: true }
+      );
+    })
+    .catch((e: any) => {
+      const err = JSON.parse(JSON.stringify(e.originalError));
 
-    if (err.status === 409) {
-      console.log("'tokenFFI' already exists in FireFly. Ignoring.");
-    } else {
-      return;
-    }
-  });  
-
+      if (err.status === 409) {
+        console.log("'tokenFFI' already exists in FireFly. Ignoring.");
+      } else {
+        return;
+      }
+    });
 
   // Listeners
   // Simple storage listener
@@ -289,7 +249,7 @@ async function init() {
     .createContractAPIListener(ssApiName, "Changed", {
       topic: "changed",
     })
-    .catch((e:any) => {
+    .catch((e: any) => {
       const err = JSON.parse(JSON.stringify(e.originalError));
 
       if (err.status === 409) {
@@ -307,7 +267,7 @@ async function init() {
     .createContractAPIListener(rewardApiName, "Transfer", {
       topic: "transfer",
     })
-    .catch((e:any) => {
+    .catch((e: any) => {
       const err = JSON.parse(JSON.stringify(e.originalError));
 
       if (err.status === 409) {
@@ -327,7 +287,7 @@ async function init() {
         events: "blockchain_event_received",
       },
     },
-    async (socket:any, event:any) => {
+    async (socket: any, event: any) => {
       console.log(
         `${event.blockchainEvent?.info.signature}: ${JSON.stringify(
           event.blockchainEvent?.output,
--- solidity/contracts/simple_storage.sol
+++ solidity/contracts/simple_storage.sol
@@ -24,42 +24,42 @@ contract SimpleStorage {
     event Changed(address indexed from, uint256 x);
 
     struct Review {
-        uint256 rating; // The rating given to the movie
-        string review;  // The textual review
-        address reviewer; // The address of the reviewer
+        uint256 rating; 
+        string review;  
+        address reviewer; 
     }
 
-    // Mapping to keep track of whether a user has reviewed a particular movie
+    
     mapping(string => mapping(address => bool)) public hasReviewed; // {movieId => {reviewerAddress => true/false}}
 
     // Mapping to store reviews for each movie
     mapping(string => Review[]) public movieReviews; // movieId -> reviews array
 
-    // Event that will be emitted when a review is added
+    
     event ReviewAdded(address indexed reviewer, string movieId, uint256 rating, string review);
 
     // Function to add a review
     function addReview(string calldata movieId, uint256 rating, string calldata review) public  {
-        // Ensure that the user has not already reviewed this movie
+        
         require(!hasReviewed[movieId][msg.sender], "User has already reviewed this movie");
 
-        // Create a new Review instance
+        
         Review memory newReview = Review({
             rating: rating,
             review: review,
             reviewer: msg.sender
         });
 
-        // Add the review to the movieReviews mapping
+        
         movieReviews[movieId].push(newReview);
-        // Mark the user as having reviewed this movie
+        
         hasReviewed[movieId][msg.sender] = true;
 
-        // Emit the ReviewAdded event
+        
         emit ReviewAdded(msg.sender, movieId, rating, review);
     }
 
-    // Function to get all reviews for a specific movie
+ 
     function getReviews(string calldata movieId) public view returns (Review[] memory) {
         return movieReviews[movieId];
     }
